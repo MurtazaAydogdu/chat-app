6c0eaa3208c73b217d86b284f58805976a924581
murtazaaydogdu <s1094770@student.hsleiden.nl>
2017-10-17 17:48:13 +0200

    Emit newMessage on craete message

--- server/server.js
+++ server/server.js
@@ -18,14 +18,14 @@ io.on('connection', function (socket) {
     console.log('new user connected');
 
 
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123
-    });
-
     socket.on('createMessage', function (message) {
         console.log('createMessage', message);
+
+        io.emit('newMessage',{
+           from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', function () {
